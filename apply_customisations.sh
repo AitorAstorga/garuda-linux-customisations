1b736b24b39fad95a6b2ef7f8c90f5f79d05bdd5
Aitor <a.astorga.sdv@gmail.com>
2023-10-09 16:52:05 +0200

    Create apply_customisations.sh

---
+++ apply_customisations.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Determine the directory where the script is being executed
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Define an associative array (similar to a dictionary in other languages) for paths
+declare -A PATHS
+
+PATHS["GRUB Themes"]="/usr/share/grub/themes/"
+PATHS["SDDM Themes"]="/usr/share/sddm/themes/"
+PATHS["Plasma Look & Feel Themes"]="/usr/share/plasma/look-and-feel/"
+PATHS["Plasma Desktop Themes"]="/usr/share/plasma/desktoptheme/"
+PATHS["Plasma Color Schemes"]="/usr/share/color-schemes/"
+PATHS["KWin Window Decorations"]="/usr/share/kwin/decorations/"
+PATHS["Plasma Layout Templates"]="/usr/share/plasma/layout-templates/"
+PATHS["Icon Themes"]="/usr/share/icons/"
+PATHS["Konsole Themes"]="/usr/share/konsole/"
+PATHS["Cursor Themes"]="/usr/share/icons/"
+PATHS["Wallpapers"]="/usr/share/wallpapers/"
+PATHS["Aurorae Window Decorations"]="/usr/share/aurorae/themes/"
+PATHS["Plasmoid Widgets"]="/usr/share/plasma/plasmoids/"
+PATHS["Kvantum Themes"]="/usr/share/Kvantum/"
+PATHS["Application Configuration"]="~/.config/"
+PATHS["Global Shortcuts"]="~/.config/kglobalshortcutsrc"
+
+# Check if root (since we are writing to system directories)
+if [[ $EUID -ne 0 ]]; then
+   echo "Please run this script as root."
+   exit 1
+fi
+
+# Iterate through each item and copy from source to destination
+for ITEM in "${!PATHS[@]}"; do
+    if [[ -d "$DIR/$ITEM" ]]; then
+        echo "Copying $ITEM..."
+        cp -r "$DIR/$ITEM" "${PATHS[$ITEM]}"
+    else
+        echo "Directory $ITEM not found in $DIR, skipping..."
+    fi
+done
+
+echo "Done copying customisations!"
