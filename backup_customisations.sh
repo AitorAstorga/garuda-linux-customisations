2b3989dd038c3562caef9104a76eba414bb4397d
Aitor <a.astorga.sdv@gmail.com>
2023-10-09 20:10:36 +0200

    Add ownership correction for user configs after copy operation.
    - Modified the script to capture the user invoking sudo.
    - After copying "Application Configuration" and "Global Shortcuts", the script now changes their ownership back to the original user to maintain proper access permissions.

--- backup_customisations.sh
+++ backup_customisations.sh
@@ -3,6 +3,9 @@
 # Determine the directory where the script is being executed
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Capture the current user's name
+CURRENT_USER=${SUDO_USER:-$USER}
+
 # Define an associative array (similar to a dictionary in other languages) for paths
 declare -A PATHS
 
@@ -20,8 +23,8 @@ PATHS["Wallpapers"]="/usr/share/wallpapers/"
 PATHS["Aurorae Window Decorations"]="/usr/share/aurorae/themes/"
 PATHS["Plasmoid Widgets"]="/usr/share/plasma/plasmoids/"
 PATHS["Kvantum Themes"]="/usr/share/Kvantum/"
-PATHS["Application Configuration"]="~/.config/"
-PATHS["Global Shortcuts"]="~/.config/kglobalshortcutsrc"
+PATHS["Application Configuration"]="$HOME/.config/"
+PATHS["Global Shortcuts"]="$HOME/.config/kglobalshortcutsrc"
 
 # Check if root (since we are reading from system directories)
 if [[ $EUID -ne 0 ]]; then
@@ -37,6 +40,11 @@ for ITEM in "${!PATHS[@]}"; do
     if [[ -d "${PATHS[$ITEM]}" || -f "${PATHS[$ITEM]}" ]]; then
         echo "Extracting $ITEM..."
         cp -r "${PATHS[$ITEM]}" "$DIR/Garuda Linux Customisations/$ITEM"
+
+        # After copying, check for the two paths and chown them back to the user
+        if [[ "$ITEM" == "Application Configuration" || "$ITEM" == "Global Shortcuts" ]]; then
+            chown -R $CURRENT_USER:$CURRENT_USER "$DIR/Garuda Linux Customisations/$ITEM"
+        fi
     else
         echo "Path for $ITEM not found, skipping..."
     fi
